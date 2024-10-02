f4557598c77ac554673b4047c27c0821b63ca1c7
Angelldca <angeluisdelcollado@gmail.com>
2024-10-02 16:58:02 -0400

    maps types

--- src/main/java/com/angelldca/customers/Service/CustomerEventService.java
+++ src/main/java/com/angelldca/customers/Service/CustomerEventService.java
@@ -1,14 +1,37 @@
 package com.angelldca.customers.Service;
 
+import com.angelldca.customers.events.CustomerCreatedEvent;
+import com.angelldca.customers.events.Event;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 
-@Service
+
+
+@Component
+@Slf4j
 public class CustomerEventService {
 
-    @KafkaListener(topics = "customers")//, groupId = "consumer-group"
-    public void listen(Object message) {
-        System.out.println("Received message: " + message);
+
+    @KafkaListener(
+            topics = "${topic.customer.name:customers}",
+            containerFactory = "kafkaListenerContainerFactory",
+            groupId = "grupo1")
+    public void consumer(Event<?> event) {
+        if (event.getClass().isAssignableFrom(CustomerCreatedEvent.class)) {
+            CustomerCreatedEvent customerCreatedEvent = (CustomerCreatedEvent) event;
+            log.info("Received Customer created event .... with Id={}, data={}",
+                    customerCreatedEvent.getId(),
+                    customerCreatedEvent.getData().toString());
+        }
     }
+  /*
+  @KafkaListener(topics = "customers", groupId = "grupo1")
+public void listenRawMessage(CustomerCreatedEvent event) {
+
+    System.out.println("Evento recibido: " + event);
+}
+  */
+
 }
--- src/main/java/com/angelldca/customers/config/CustomerConfig.java
+++ src/main/java/com/angelldca/customers/config/CustomerConfig.java
@@ -2,21 +2,25 @@ package com.angelldca.customers.config;
 
 
 import com.angelldca.customers.events.Event;
-import org.apache.kafka.clients.admin.NewTopic;
+
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
+
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.TopicBuilder;
+
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -43,21 +47,24 @@ public class CustomerConfig {
 
     @Bean
     public ConsumerFactory<String, Event<?>> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(
+         JsonDeserializer<Event<?>> jsonDeserializer = new JsonDeserializer<>();
+         jsonDeserializer.trustedPackages("*");
+
+
+        return new DefaultKafkaConsumerFactory(
                 consumerConfigs(),
                 new StringDeserializer(),
-                new JsonDeserializer<>(Event.class)); // Deserializa a Event
+                new ErrorHandlingDeserializer<>(jsonDeserializer)); // Deserializa a Event
     }
 
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);  // Configura el group.id
-        props.put(JsonSerializer.TYPE_MAPPINGS, "com.angelldca:com.angelldca.customers.events.Event");
-
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName());
+        props.put(JsonSerializer.TYPE_MAPPINGS, "com.angelldca.producer.events.CustomerCreatedEvent:com.angelldca.customers.events.CustomerCreatedEvent");
 
         return props;
     }
